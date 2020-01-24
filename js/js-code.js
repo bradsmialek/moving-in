7f37068978f00cdc2f18bc7a347a25eb6cc8607b
josephlee3454 <joseph.lee3454@gmail.com>
2020-01-23 22:52:37 -0800

    js updated

--- js/js-code.js
+++ js/js-code.js
@@ -3,10 +3,17 @@ function password() {
 
 var password = prompt("please enter pin")
 var pin;
-if (password === "6767"){
+while ( password !== '6767' && password != 'joe')
+{
+    password = prompt ('please enter password or pin')
+};
+if (password === "6767")
+{
     
     pin = "pin accepted"
-
+}
+else if (password === "joe"){
+    pin = 'password accepted'
 }
 else {
     window.location = 'https://josephlee3454.github.io/example';
@@ -14,60 +21,23 @@ else {
 return '<h3>' + pin + '</h3>' ;
 }
 
-
-
-
-
-
-var country = prompt("what country are you fromm", "county here...")
-var greetings;
-
-if(country === "America" || country === "usa") {
-    greetings = 'Hello there! welcome!';
-
-} else if (country === "mexico" || country === "mx") {
-    greetings = 'Hola! que gusto que nos visites!';
-} else {
-    greetings = 'so glad you are here!';
+function movers()
+{
+movers = pompt("how many movers");
+while(movers===0)||(movers !== Number())
+{
+movers = prompt("how many movers")
+}
+total = 0 
+cost = 50 
+for (var i = 0; i<movers; i++)
+return '<h3>' + movers + '</h3>';
 }
 
-document.write('<h3>' + greetings + '</h3>');
 
 
-var salary = prompt("how much is your salary")
-var money;
- if(salary > 60000) 
- {
-     money = 'afordable'; 
-    }
-    else if (salary < 60000)
-    {
-        money = 'unaffordable';
 
-    }
-    document.write('<h5>' + money + '</h5>')
-     
-  var weather = prompt ("what season are you moving")  
-  var season;
-  if (weather === "winter")
-  {
-      season = 'It rains alot so bring a umbrella';
-  } else if (weather === "summer")
-    {
-        season = 'The summers are so beautiful prepare for lots of sun';
-    }
-    else if (weather === "spring")
-    {
-        season = 'the rain starts to go away and the flowers bloom';
-    }
-   else if (weather === "fall" || weather === "autumn")
-   {
-       season = 'The leaves are so big and beautiful in fall';
 
-   }
-
-   document.write('<h5>' + season + '<h5>');
-
-   
+    
 
 
